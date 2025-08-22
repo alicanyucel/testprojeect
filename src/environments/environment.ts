998ccd842cd56d03f93c885ba15fdc2d2a75d1d6
Ali Can Yücel <32604562+alicanyucel@users.noreply.github.com>
2025-08-22 12:37:44 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,3 @@
-export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyDx49o0-fM7r62fyUCifeTG4vMeFKXADMQ",
-    authDomain: "truckport-live.firebaseapp.com",
-    databaseURL: "https://truckport-live-default-rtdb.firebaseio.com",
-    projectId: "truckport-live",
-    storageBucket: "truckport-live.firebasestorage.app",
-    messagingSenderId: "771802775117",
-    appId: "1:771802775117:web:bbdafa67d1aebbf244a604",
-    measurementId: "G-EMXG24NCNT"
-  }
-};
+
+
+
